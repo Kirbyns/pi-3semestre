ff29cb1635a5a9e0b33974a37028744317d043aa
matheus Oliveira <moasaraujo@gmail.com>
2022-03-23 22:04:21 -0300

    adicionando as modificações do professor

--- resources/views/category/create.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{route('category.store')}}" method="POST">
-@csrf
-Nome da categoria <input type ="text" name="name">
-<button type ="submit"> Enviar </button>
-
-</form>
--- resources/views/category/edit.blade.php
+++
@@ -1,8 +0,0 @@
-<form action="{{route('category.update',$category->id)}}" method="POST">
-@csrf
-@method('PUT')
-Nome da categoria <input type ="text" name="name" value="{{$category->name}}">
-
-<button type ="submit"> Enviar </button>
-
-</form>
